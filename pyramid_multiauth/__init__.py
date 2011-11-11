3ab53a4a7c1130a0f941654a012f020348a4358c
Ryan Kelly <ryan@rfk.id.au>
2011-11-11 16:10:40 +1100

    add some versioning metadata

--- pyramid_multiauth/__init__.py
+++ pyramid_multiauth/__init__.py
@@ -37,6 +37,14 @@
 Pyramid authn policy that ties together multiple backends.
 """
 
+__ver_major__ = 0
+__ver_minor__ = 1
+__ver_patch__ = 0
+__ver_sub__ = ""
+__ver_tuple__ = (__ver_major__, __ver_minor__, __ver_patch__, __ver_sub__)
+__version__ = "%d.%d.%d%s" % __ver_tuple__
+
+
 from zope.interface import implements
 
 from pyramid.interfaces import IAuthenticationPolicy, PHASE2_CONFIG
