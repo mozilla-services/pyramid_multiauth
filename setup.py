42b285ef03e9f1b09c2b37803b86bb88e4ecde2a
Ryan Kelly <rkelly@mozilla.com>
2013-05-15 08:32:45 +1000

    Bump to dev version after release

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ if sys.version_info < (2, 7):
     requires.append("unittest2")
 
 setup(name='pyramid_multiauth',
-      version='0.3.1',
+      version='0.3.2dev1',
       description='pyramid_multiauth',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
