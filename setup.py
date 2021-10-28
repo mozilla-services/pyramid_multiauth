624124a5e2832183b961c663554a2e0dde966599
Mathieu Leplatre <mathieu@mozilla.com>
2021-10-28 11:11:53 +0200

    Preparing release 1.0.1

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open(os.path.join(here, 'CHANGES.txt')) as f:
 requires = ['pyramid>=2,<3']
 
 setup(name='pyramid_multiauth',
-      version='1.0.1.dev0',
+      version='1.0.1',
       description='pyramid_multiauth',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
